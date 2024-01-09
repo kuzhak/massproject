d7067f372988c45f56255bbad430dea78b39c15d
Nikolay <kujak73@outlook.com>
2024-01-09 16:33:01 +0400

    add migration requests

--- database/migrations/2024_01_08_200821_create_request_table.php
+++ database/migrations/2024_01_08_200821_create_request_table.php
@@ -11,14 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('requests', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->enum('role', ['user', 'responsible', 'admin']);
-            $table->rememberToken();
+            $table->string('email');
+            $table->enum('status', ['active', 'resolved'])->default('active');
+            $table->text('message');
+            $table->text('comment');
             $table->timestamps();
         });
     }
@@ -28,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('requests');
     }
 };
